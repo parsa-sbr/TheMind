856b952786e21d9b6faafedb91bcbae36a777352
parsa__sbr <parsa.sabrian22@gmail.com>
2022-07-27 14:33:18 +0430

    completing ClientHandler class and editing game and server classes

--- src/main/java/model/ClientHandler.java
+++ src/main/java/model/ClientHandler.java
@@ -1,4 +1,93 @@
 package model;
 
-public class ClientHandler {
+import server.Game;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class ClientHandler extends Thread{
+
+    private Socket socket;
+    private Scanner in;
+    private PrintWriter out;
+    private Vector<Integer> cards;
+    private Game game;
+    private String username;
+
+    public ClientHandler(Socket socket, String username) {
+        this.socket = socket;
+        this.username = username;
+        cards = new Vector<>();
+        try {
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            String input = in.nextLine();
+            try {
+                int playedCard = Integer.parseInt(input);
+                if (cards.contains(playedCard)) {
+                    //send card to game
+                    game.playCard(username, playedCard);
+                }
+                else {
+                    out.println("you don't have this card!");
+                    out.flush();
+                }
+            } catch (Exception e) {
+                if (input.equals("ninja")) {
+                    //play the ninja card
+                    game.playCard(username, -2);
+                }
+                else if (input.contains(":)")) {
+                    //send :) emoji
+                    game.sendToAll(username, ":)");
+                }
+                else if (input.contains(":(")) {
+                    //send :( emoji
+                    game.sendToAll(username, ":(");
+                }
+                else if (input.contains(":|")) {
+                    //send :| emoji
+                    game.sendToAll(username, ":|");
+                }
+                else if (input.equals("exit")) {
+                    //close the socket and add a bot instead of the player
+
+                    //game.removePlayer(this);
+                }
+            }
+        }
+    }
+
+    public void sendMessage(String text) {
+        out.println(text);
+        out.flush();
+    }
+
+    public Vector<Integer> getCards() {
+        return cards;
+    }
+
+    public void setCards(Vector<Integer> cards) {
+        this.cards = cards;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,10 +1,14 @@
 package server;
 
+import model.Bot;
+import model.ClientHandler;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Server {
 
@@ -17,6 +21,9 @@ public class Server {
     public void init() throws IOException {
         System.out.println("server started.");
         while (true) {
+            Vector<ClientHandler> clientHandlers = new Vector<>();
+            Vector<Bot> bots = new Vector<>();
+
             System.out.println("Waiting for a host...");
 
             Socket socket = serverSocket.accept();
@@ -25,17 +32,34 @@ public class Server {
             Scanner in = new Scanner(socket.getInputStream());
             PrintWriter out = new PrintWriter(socket.getOutputStream());
 
-            out.println("Hello, you're the host.\nhow many players does game have? ");
+            out.println("Hello, you're the host.\nWhat is your name? ");
+            out.flush();
+
+            String name = in.nextLine();
+            ClientHandler host = new ClientHandler(socket, name);
+            clientHandlers.add(host);
+
+            out.println("How many players does the game have?");
             out.flush();
             int numberOfPlayers = Integer.parseInt(in.nextLine());
 
             for (int i = 0; i < numberOfPlayers - 1; i++) {
                 Socket tempSocket = serverSocket.accept();
                 System.out.println("A player joined.");
+                Scanner tempIn = new Scanner(tempSocket.getInputStream());
+                PrintWriter tempOut = new PrintWriter(tempSocket.getOutputStream());
+
+                tempOut.println("Hello, What is your name? ");
+                tempOut.flush();
+                name = tempIn.nextLine();
 
                 //adding player to a list
+                ClientHandler player = new ClientHandler(tempSocket, name);
+                clientHandlers.add(player);
 
-                out.println("there are " + (i + 2) + " players in the server including you, do want to start the game? ");
+
+
+                out.println("there are " + clientHandlers.size() + " players in the server including you, do want to start the game? ");
                 out.flush();
                 String response = in.nextLine();
                 if (response.equals("yes")) {
