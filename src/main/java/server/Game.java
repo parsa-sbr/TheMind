ef2fcd3cd0d44d8060d58392df0f493c0ffcd30d
parsa__sbr <parsa.sabrian22@gmail.com>
2022-07-29 20:11:17 +0430

    getting ready for merge

--- src/main/java/server/Game.java
+++ src/main/java/server/Game.java
@@ -1,44 +1,4 @@
 package server;
 
-import model.Bot;
-import model.ClientHandler;
-
-import java.util.Vector;
-
 public class Game extends Thread{
-
-    Vector<ClientHandler> clientHandlers;
-    Vector<Bot> bots;
-    Vector<Integer> deckOfAll;
-    int cardOnTable;
-    Integer playedCard = -1;
-
-
-
-
-
-    public void sendToAll(String username, String message) {
-        for (ClientHandler c : clientHandlers) {
-            if (!c.getUsername().equals(username)) {
-                c.sendMessage(username + ": " + message);
-            }
-        }
-    }
-
-    public void playCard(String username, int card) {
-        playedCard = card;
-
-        String played = String.valueOf(card);
-        if (card == -2)
-            played = "ninja";
-        sendToAll(username, "played card " + played);
-    }
-
-    public int getCardOnTable() {
-        return cardOnTable;
-    }
-
-    public void setCardOnTable(int cardOnTable) {
-        this.cardOnTable = cardOnTable;
-    }
 }
